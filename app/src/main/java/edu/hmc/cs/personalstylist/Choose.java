dcad917072ce87c3704cbdf72c5bab3e2d928041
dlc02011 <david.connor@pomona.edu>
2014-10-14 14:28:46 -0700

    commit before choose class push

--- app/src/main/java/edu/hmc/cs/personalstylist/Choose.java
+++ app/src/main/java/edu/hmc/cs/personalstylist/Choose.java
@@ -3,41 +3,80 @@ package edu.hmc.cs.personalstylist;
 
 import java.util.Iterator;
 import java.util.ArrayList;
-import Wardrobe;
+import edu.hmc.cs.personalstylist.Wardrobe;
+import java.util.Random;
 
 /**
  * Created by davidconnor on 10/10/14.
+ *  This class is intended to take user's preferences to return viable clothing options and
+ *  a #1 choice outfit.
  */
 public class Choose {
-}
 
 
-    // The ArrayList that contains the articles of clothing
-    ArrayList<ViableClothes> clothingList = new ArrayList<ViableClothes>();
-
-
-
-    /**
-     * Eliminates non-viable clothing from list of viable tops
-     *
-     * @param name The name of the article of clothing to remove
-     * @return
-     */
-    public Clothing removeItems(list articleList, string formalityPref, string weatherPref)  {
-        for (int i = 0; i < articleList.size(); ++i) {
-            if (articleList.get(i).formality != formalityPref) {
-                articleList.remove(i);
-                --i;
-            else if (articleList.get(i).temperature != weatherPref) {
-                articleList.remove(i);
-                --i;
-            }
-        return null;
-
- }
         /**
-         * Eliminates non-viable clothing from a  list of viable bottoms
+         * Eliminates non-viable clothing from list of bottoms, returns the remaining list
          *
-         * @param name The name of the article of clothing to remove
-         * @return
+         * @param articleList, formalityPref, weatherPref
+         * @return ArrayList of viableBottoms
          */
+    public ArrayList removeTopsItems(ArrayList articleList, Wardrobe.ClothingFormality formalityPref, Wardrobe.ClothingTemperature weatherPref) {
+        ArrayList viableTops = null;
+        for (int i = 0; i < articleList.size(); ++i) {
+            Wardrobe.Clothing clothingArticle = (Wardrobe.Clothing) articleList.get(i);
+            if (clothingArticle.formality == formalityPref &&
+                    clothingArticle.temperature == weatherPref) {
+                viableTops.add(clothingArticle);
+            }
+        }
+            return viableTops;
+
+    }
+
+        /**
+         * Eliminates non-viable clothing from list of bottoms, returns remaining list
+         *
+         * @param  articleList, formalityPref, weatherPref
+         * @return ArrayList of viableBottoms
+         */
+
+
+    public ArrayList removeBottomsItems(ArrayList articleList, Wardrobe.ClothingFormality formalityPref, Wardrobe.ClothingTemperature weatherPref) {
+        ArrayList viableBottoms = null;
+        for (int i = 0; i < articleList.size(); ++i) {
+            Wardrobe.Clothing clothingArticle = (Wardrobe.Clothing) articleList.get(i);
+            if (clothingArticle.formality == formalityPref &&
+                    clothingArticle.temperature == weatherPref) {
+                viableBottoms.add(clothingArticle);
+            }
+        }
+            return viableBottoms;
+
+    }
+
+
+        /**
+         * Returns ArrayList of Items in Top Choice Outfit (1 top, 1 bottom)
+         *
+         * @param viableTops, viableBottoms
+         * @return ArrayList outfit
+         */
+
+    public ArrayList topOutfit(ArrayList viableTops, ArrayList viableBottoms) {
+        ArrayList outfit = null;
+        Random generator = new Random();
+
+        int i = generator.nextInt(viableTops.size());
+        Wardrobe.Clothing topTop;
+        topTop = (Wardrobe.Clothing) viableTops.get(i);
+        outfit.add(topTop);
+
+        int j = generator.nextInt(viableBottoms.size());
+        Wardrobe.Clothing topBottom;
+        topBottom = (Wardrobe.Clothing) viableBottoms.get(i);
+        outfit.add(topBottom);
+
+        return outfit;
+
+    }
+}
